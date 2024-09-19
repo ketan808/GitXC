939e75127066fc1355c4648029b2bb0497de996d
Ketan Patel <design@xtracreative.co.uk>
2024-09-19 17:27:59 +0100

    tchange

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -26,7 +26,7 @@ const About = () => {
  <Grid.Col span={{ base: 12, sm: 'auto', md: 'auto', lg: 'auto' }} >
 
 {/* <span>Digital design came to me when a family friend saw my drawings and bought me an online web-design course, which I completed with enthusiasm and discovered design was not just a passion but a calling.</span> */}
-<span>I started my design career by developing websites for local businesses, which led to my first major break with a design agency called Clock. It was a wonderful opportunity for me to work directly on websites for organisations such as JD Wetherspoon and PBJ management.</span>
+<span>I started my design career over 20 years ago by developing websites for local businesses, which led to my first major break with a design agency called Clock. It was a wonderful opportunity for me to work directly on websites for organisations such as JD Wetherspoon and PBJ management.</span>
 <p>In a chance meeting, I accepted a web-design role for Combined Insurance (AON), where I redesigned the UI and UX for all their organisation websites and got a taste for some coding. The role taught me how to listen attentively and identify customer pain points that could inform effective design decisions.</p>
 <p>The rest is history; since then, I've been designing and expanding my toolkit. I love working across the full spectrum of product design, from research through to UI and implementation, and get a ton of energy from working on seriously impactful work, such as in my last contract for the British Army.</p>
 <p>I hope you enjoy my portfolio, and would love to connect if there's an opportunity to work together.</p>
