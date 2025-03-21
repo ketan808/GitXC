b88f92fe28d08ad18185ffa4bb6b45f133935ba9
Ketan Patel <design@xtracreative.co.uk>
2025-03-21 13:32:53 +0000

    push me

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ return (
 <div>
   
 
-<BigTitle title1='KETAN PATEL' title2='Hi! Im a London Based UI UX Designer' title3='I love designing simple solutions to complex problems.' />
+<BigTitle title1='KETAN PATEL' title2='Hi! Im a London Based Product Designer' title3='I love designing simple solutions to complex problems.' />
 
 
     
