4f7623826aa1de55fe76637ce9a7c52d706719bd
Ketan Patel <design@xtracreative.co.uk>
2025-05-02 14:05:55 +0100

    new change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ return (
 <div>
   
 
-<BigTitle title1='KETAN PATEL' title2='Strategic Designer Who Loves Big Data and Is Curious About AI' title3='I research and deliver simple solutions to complex problems by implementing design systems to UI with Prototypes and Code.' />
+<BigTitle title1='KETAN PATEL' title2='Strategic Designer Who Loves Big Data and Is Curious About AI' title3='I research and deliver simple solutions to complex problems by applying design systems to UI using prototypes and code.' />
 
 
     
