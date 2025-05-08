e626e1f4b7f020f6edb8975fa324545f2164636a
Ketan Patel <design@xtracreative.co.uk>
2025-05-08 12:06:41 +0100

    flare

--- src/components/BigTitleArmy.tsx
+++ src/components/BigTitleArmy.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Title, Container, SimpleGrid, Box, Image} from '@mantine/core';
 import TextReveal from '../inViewUI/TextRevealStop';
 import image1 from '../images/army/army_header.png';
+import bgImage from '../images/flare/green.png';
 
 interface TitlesProps {
   title1: string;
@@ -11,65 +12,43 @@ interface TitlesProps {
 
 const Titles: React.FC<TitlesProps> = ({ title1, title2, title3 }) => {
   return (
-
-<div className=''>
-<Container size="xl"
-mb={{ base: '20px'}}
-
->
-
-<SimpleGrid
-cols={{ base: 1, sm: 1, md: 1, lg: 1, xl: 2 }}
-spacing="xl" 
->
-
-
-<Box
-mt={{ base: '40px' }}
-mb={{ base: '0px', xl:'40px' }}
->   
-
-
-<Title className='theme0' order={3} fw="600">{title1}</Title>
-
-
-<Title className='theme0'
-
-lh={{ base: 1.1 }}
-fz={{ base: 50, sm: 50, md: 60, lg: 60, xl: 70  }}
-mt={{ base: 20, sm: 20, md: 20, lg: 40 }}
-mb={{ base: 20, sm: 20, md: 20, lg: 40 }}
->
-<TextReveal text={title2} />
-</Title>
-
-
-<Title className='theme0' order={4} fw="500">{title3}</Title>
-
-</Box>
-
-
-
-<Box
-mt={{ base: '0px', lg: '0px', xl: '40px' }}
-mb={{ base: '0px', lg: '0px', xl: '40px' }}
-> 
-<Image src={image1} alt="Army Image" />
-
-</Box>
-
-
-
-</SimpleGrid>
-
-
-</Container>
-
-</div>
-
-
-
-
+    <div style={{
+      position: 'relative',
+      width: '100%',
+      backgroundImage: `url(${bgImage})`,
+      backgroundSize: 'contain',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+    }}>
+      <Container size="xl" mb={{ base: '20px' }}>
+        <SimpleGrid
+          cols={{ base: 1, sm: 1, md: 1, lg: 1, xl: 2 }}
+          spacing="xl" 
+        >
+          <Box
+            mt={{ base: '40px' }}
+            mb={{ base: '0px', xl:'40px' }}
+          >   
+            <Title className='theme0' order={3} fw="600">{title1}</Title>
+            <Title className='theme0'
+              lh={{ base: 1.1 }}
+              fz={{ base: 50, sm: 50, md: 60, lg: 60, xl: 70  }}
+              mt={{ base: 20, sm: 20, md: 20, lg: 40 }}
+              mb={{ base: 20, sm: 20, md: 20, lg: 40 }}
+            >
+              <TextReveal text={title2} />
+            </Title>
+            <Title className='theme0' order={4} fw="500">{title3}</Title>
+          </Box>
+          <Box
+            mt={{ base: '0px', lg: '0px', xl: '40px' }}
+            mb={{ base: '0px', lg: '0px', xl: '40px' }}
+          > 
+            <Image src={image1} alt="Army Image" />
+          </Box>
+        </SimpleGrid>
+      </Container>
+    </div>
   );
 };
 
--- src/components/HeroCrystal.tsx
+++ src/components/HeroCrystal.tsx
@@ -10,7 +10,7 @@ return (
 <div>
   
 
-<BigTitle title1='CRYSTAL SKI' title2='User Interface & Visual Design for a Complete Brand Refresh' title3='Crystal ski holidays have over 100 resorts across Europe and North America and have long been established as one of the UKs leading tour operators.' />
+<BigTitle title1="CRYSTAL SKI" title2='User Interface & Visual Design for a Complete Brand Refresh' title3='Crystal ski holidays have over 100 resorts across Europe and North America and have long been established as one of the UKs leading tour operators.' />
 
 
     
